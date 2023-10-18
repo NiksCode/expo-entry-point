8e5ebbda0d19c33d507695d87c4dde40fc5a6547
Nik Polovenko <polovenko.nikita@gmail.com>
2023-10-18 17:21:51 +0200

    commit with entry point

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome to your new app!</Text>
+    </View>
+  );
+};
+
+export default App;
